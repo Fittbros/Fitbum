40cd20cc30854757ca9cfb7472d1adf07d6b1215
7Teen17T <59067294+7Teen17T@users.noreply.github.com>
2023-03-19 18:00:30 +0100

    import java.time.LocalDate;

--- src/main/java/com/fitbum/Fitbum/entidades/Content_tutorial.java
+++ src/main/java/com/fitbum/Fitbum/entidades/Content_tutorial.java
@@ -2,7 +2,9 @@ package com.fitbum.Fitbum.entidades;
 
 public class Content_tutorial {
 
-    
+private String nombre, descripcion, url;
+
+
 
 
 }
--- src/main/java/com/fitbum/Fitbum/entidades/Notificacion.java
+++ src/main/java/com/fitbum/Fitbum/entidades/Notificacion.java
@@ -1,4 +1,14 @@
 package com.fitbum.Fitbum.entidades;
-
+import java.time.LocalDate;
 public class Notificacion {
+
+    private LocalDate fecha_envio;
+    private String texto;
+    private boolean leido, eliminar;
+
+
+
+
+
+
 }
