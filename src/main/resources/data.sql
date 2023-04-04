3e309ccbca4f3b4f1a9389fedaae530954fd4cf5
7Teen17T <59067294+7Teen17T@users.noreply.github.com>
2023-04-04 20:30:21 +0200

    abriendo index

---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+-- # insert into Country value(1, 'Abuja', 'NGN', 'Africa', 'Nigeria', 'American');
+-- # insert into Country value (2, 'New Delhi', 'IN', 'North America', 'United State', 'American');
+-- # insert into Country value (3, 'Budapest', 'HUN', 'Europe', 'United State', 'American');
+-- # insert into Country value (4, 'Budapest', 'HUN', 'North America', 'United State', 'American');
+
