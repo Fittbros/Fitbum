38d386d372b991d6b2000033739b065167bd85e3
7Teen17T <59067294+7Teen17T@users.noreply.github.com>
2023-04-09 20:11:58 +0200

    Merge branch 'master' of https://github.com/Fittbros/Fitbum

--- src/main/java/com/fitbum/Fitbum/FitbumApplication.java
+++ src/main/java/com/fitbum/Fitbum/FitbumApplication.java
@@ -1,13 +1,28 @@
 package com.fitbum.Fitbum;
 
+import com.fitbum.Fitbum.entidades.DetalleUsuario;
+import com.fitbum.Fitbum.repositories.DetalllesRepositorio;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 @SpringBootApplication
 public class FitbumApplication {
 
+	@Autowired
+	DetalllesRepositorio detalllesRepositorio;
+
+
+
 	public static void main(String[] args) {
 		SpringApplication.run(FitbumApplication.class, args);
 	}
+
+
+
 
 }
--- src/main/java/com/fitbum/Fitbum/programa/controllers/SesionController.java
+++ src/main/java/com/fitbum/Fitbum/programa/controllers/SesionController.java
@@ -1,16 +1,6 @@
 package com.fitbum.Fitbum.controladores;
-import jakarta.persistence.*;
 
-@Entity
-@Table(name="Sesion")
 public class SesionController {
 
-    @Id
-    @GeneratedValue(strategy= GenerationType.AUTO)
-    private int idSesion;
 
-    private int idMicrociclo;
-
-    private String nombre_variante;
-    private String descripcion;
 }
