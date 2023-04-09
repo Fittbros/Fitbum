3241e6d823a3ecaf84ebb62446dfde359ea1e2c1
7Teen17T <59067294+7Teen17T@users.noreply.github.com>
2023-04-09 20:29:34 +0200

    ?

--- src/main/java/com/fitbum/Fitbum/FitbumApplication.java
+++ src/main/java/com/fitbum/Fitbum/FitbumApplication.java
@@ -1,11 +1,23 @@
 package com.fitbum.Fitbum;
 
+<<<<<<< Updated upstream
+=======
+import com.fitbum.Fitbum.entidades.DetalleUsuario;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+>>>>>>> Stashed changes
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication(exclude = {org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class})
 public class FitbumApplication {
 
+<<<<<<< Updated upstream
+=======
+	@Autowired
+
+
+>>>>>>> Stashed changes
 	public static void main(String[] args) {
 		SpringApplication.run(FitbumApplication.class, args);
 	}
