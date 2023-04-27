4ecd65062c4d45ca3e7a2ca2761a82100c7832af
ttxus13 <64156714+ttxus13@users.noreply.github.com>
2023-04-27 18:04:01 +0200

    Update data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -6,14 +6,11 @@ insert into detalle_usuario values (1, 1.65,'Pere','Manzana','Mad@rid','1990-01-
                                    (4, 1.73,'Maresca','Perez','Mad@rid','1995-01-01','marco','1234', 55,  1);
 
 insert into suscripcion values (1,'aa','1990-01-01','1990-01-01',50,1),(2,'bb','1990-01-01','1990-01-01',60,1),(3,'cc','1990-01-01','1990-01-01',45,2);
-delete from suscripcion_seq;
-insert into suscripcion_seq values (4);
+
 insert into relaciones values (1,'2025-01-01','1990-12-01',3,2),(2,'2023-01-01','2001-06-01',4,2);
-delete from relaciones_seq;
-insert into relaciones_seq values (3);
+
 insert into ejercicios values (1,'tocarse','Brazo','+1 rep +1 peso','Ejercicio 1'),(2,'saltar','pierna','+2 rep','Jump');
-delete from ejercicios_seq;
-insert into ejercicios_seq values (3);
+
 insert into content_tutorial values (1,'tocarse fuerte','Ejercicio 1','https://www.youtube.com/watch?v=Jv4O_XeHPyA&pp=ygUjY2FiYWxsbyBob21vc2V4dWFsIGRlIGxhcyBtb250YcOxYXM%3D'),(2,'saltar muy alto','jump','https://www.youtube.com/watch?v=Jv4O_XeHPyA&pp=ygUjY2FiYWxsbyBob21vc2V4dWFsIGRlIGxhcyBtb250YcOxYXM%3D');
 
 
