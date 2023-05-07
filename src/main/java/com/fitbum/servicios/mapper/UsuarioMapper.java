dbd45071d166c186e3c0db876700337580180be0
Dartonico <123487092+Dartonico@users.noreply.github.com>
2023-05-07 17:41:55 +0200

    cambios del mapeo usuario

--- src/main/java/com/fitbum/servicios/mapper/UsuarioMapper.java
+++ src/main/java/com/fitbum/servicios/mapper/UsuarioMapper.java
@@ -1,4 +1,37 @@
 package com.fitbum.servicios.mapper;
 
+import com.fitbum.dto.UsuarioDto;
+import com.fitbum.dto.UsuarioDtoPsw;
+import com.fitbum.entidades.usuarios.Usuario;
+import org.modelmapper.ModelMapper;
+
 public class UsuarioMapper {
+    public UsuarioDto mapToUserDto(Usuario user){
+        UsuarioDto userDto = new UsuarioDto();
+        ModelMapper modelMapper = new ModelMapper();
+        modelMapper.map(user,userDto);
+        userDto.setNombreUsuario(user.getNombre()+" "+user.getApellido1()+" "+user.getApellido2());
+        return userDto;
+    }
+    public UsuarioDtoPsw mapToUserDtoPsw(Usuario user){
+        UsuarioDtoPsw userDto = new UsuarioDtoPsw();
+        ModelMapper modelMapper = new ModelMapper();
+        modelMapper.map(user,userDto);
+        userDto.setNombreUsuario(user.getNombre()+" "+user.getApellido1()+" "+user.getApellido2());
+        return userDto;
+    }
+
+    public Usuario mapToUserPsw(UsuarioDtoPsw usuarioDtoPsw){
+        Usuario user = new Usuario();
+        ModelMapper modelMapper = new ModelMapper();
+        modelMapper.map(usuarioDtoPsw,user);
+        return user;
+    }
+
+    public Usuario mapToUser(UsuarioDto usuarioDto){
+        Usuario user = new Usuario();
+        ModelMapper modelMapper = new ModelMapper();
+        modelMapper.map(usuarioDto,user);
+        return user;
+    }
 }
